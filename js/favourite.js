008ab714a5e768b785eddb24a3e2e886ac88aa8d
Helani Waidyarathne <35195335+HelaniKathyana@users.noreply.github.com>
2021-01-15 02:40:18 +0530

    favourite

--- js/favourite.js
+++ js/favourite.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
         this.checked = false;
       });
       removeAllselectedProducts();
-      $(".favourite-button").css('background-color', '#1E1E1E');
+      $(".favourite-button").css('background-color', '#4caf50');
       $(".favourite-button").css('pointer-events', 'none');
     }
   });
@@ -115,14 +115,14 @@ function displayFavourites() {
       for (j = 0; j < favouriteProducts.length; j++) {
 
         if (products[i].id == favouriteProducts[j]) {
-          output += '<li><div class="item-container">';
+          output += '<div class="item-container">';
           output += '<div class="card item-card">'
           output += '<div class="item-image-container" style="background-image: url(' + products[i].image + ')"></div>';
           output += '<div class="item-details"><div>';
           output += '<div class="item-header-container"><strong class="item-header-text"><b>' + products[i].name + '</b></strong></div>';
           output += '<div><p>' + products[i].details + '</p></div>';
           output += '<div><p><b>' + products[i].price + '</b></p></div>';
-          output += '<div class="favourite-product-details" style="position: absolute;top: 0;right: 0;color: #4CAF50; padding-top: 10px;"><label class="favourite-product-checkbox"><input type="checkbox" name="" id="productCheckBox" data-role="none"><span class="checkmark"></span></label></div><div class="favourite-icon" style="position: absolute;bottom: 0;right: 0;color: #4CAF50;"><div class="footer-action"> <i class="fas fa-heart" style="font-size: 24px;"></i></div></div></div></div></div></li>';
+          output += '<div class="favourite-product-details" style="position: absolute;top: 0;right: 0;color: #4CAF50; padding-top: 10px;"><label class="favourite-product-checkbox"><input type="checkbox" name="" id="productCheckBox" data-role="none"><span class="checkmark"></span></label></div><div class="favourite-icon" style="position: absolute;bottom: 0;right: 0;color: #4CAF50;"><div class="footer-action"> <i class="fas fa-heart" style="font-size: 24px;"></i></div></div></div></div></div>';
         }
       }
     }
