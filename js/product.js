9646baad632a86d2455fcfc8609e57557328e9bc
Helani Waidyarathne <35195335+HelaniKathyana@users.noreply.github.com>
2021-01-17 21:20:51 +0530

    product

---
+++ js/product.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    $('#product_favourite1').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+    $('#product_favourite2').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+    $('#product_favourite3').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+    $('#product_favourite4').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+    $('#product_favourite5').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+    $('#product_favourite6').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+    $('#product_favourite7').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+    $('#product_favourite8').on('click',function() {
+        $(this).find("i").toggleClass("far fas selected-heart border-heart");
+    });
+
+});
